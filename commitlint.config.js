19e395e9dd71c43e424aeabe6b7f598218c40ab3
白宇东 <summer88123@163.com>
2026-02-27 13:42:27 +0800

    chore: 添加基础配置

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // 新功能
+        'fix',      // Bug 修复
+        'docs',     // 文档更新
+        'style',    // 代码格式（不影响代码运行的变动）
+        'refactor', // 重构（既不是新增功能，也不是修复 bug）
+        'perf',     // 性能优化
+        'test',     // 增加测试
+        'chore',    // 构建过程或辅助工具的变动
+        'revert',   // 回退
+        'build',    // 构建系统或外部依赖的变动
+        'ci',       // CI 配置文件和脚本的变动
+      ],
+    ],
+    'subject-case': [0], // 不限制 subject 大小写
+  },
+};
